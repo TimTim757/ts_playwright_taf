83444469a8e1f7ae26658d4585f0d8d66b08423d
timofei_kolosov@epam.com <timofei_kolosov@epam.com>
2026-02-08 02:36:59 +0400

    feat: page object and shared account
    task-1
    
    - add address of share account to playwright.config.ts
    - implement page object for existing tests

---
+++ pages/OpenNewAccountPage.ts
@@ -0,0 +1,47 @@
+import { Locator, Page, expect } from '@playwright/test';
+
+export class OpenNewAccountPage {
+    readonly page: Page;
+
+    readonly openNewAccountLink: Locator;
+    readonly accountTypeSelect: Locator;
+    readonly openAccountButton: Locator;
+    readonly newAccountNumber: Locator;
+    readonly accountOpenedHeading: Locator;
+    readonly successMessage: Locator;
+    readonly newAccountMessage: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.accountTypeSelect = page.locator('#type');
+        this.openNewAccountLink = page.getByRole('link', { name: 'Open New Account' });
+        this.openAccountButton = page.locator('input.button[value="Open New Account"]');
+        this.newAccountNumber = page.locator('#newAccountId');
+        this.accountOpenedHeading = page.getByRole('heading', { name: 'Account Opened!' });
+        this.successMessage = page.getByText('Your new account number');
+        this.newAccountMessage = page.locator('#openAccountResult');
+    }
+    async goto() {
+        await this.openNewAccountLink.click();
+    }
+
+    async selectAccountType(accountType: string) {
+        await this.accountTypeSelect.selectOption(accountType);
+    }
+
+    async submitForm() {
+        await this.openAccountButton.click();
+    }
+
+  async getNewAccountNumber() {
+    await this.newAccountNumber.waitFor({state: 'visible'});
+    return await this.newAccountNumber.textContent();
+  }
+
+  async verifyAccountOpened(newAccountNumber: string) {
+    await expect(this.accountOpenedHeading).toBeVisible();
+    await expect(this.successMessage).toBeVisible();
+    await expect(this.newAccountMessage).toBeVisible();
+    await expect(this.newAccountMessage).toContainText(newAccountNumber);
+  }
+}   
